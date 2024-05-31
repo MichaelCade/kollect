c6112eac56864b540088e4ec41cae382c0d82070
MichaelCade <michael.cade@outlook.com>
2024-05-31 15:15:22 +0100

    Initial commit

--- cmd/kollect/main.go
+++ cmd/kollect/main.go
@@ -9,17 +9,10 @@ import (
 	"path/filepath"
 
 	"github.com/michaelcade/kollect/pkg/kollect"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatalf("Failed to build kubeconfig: %v", err)
-	}
 
 	http.Handle("/", http.FileServer(http.Dir("web")))
 	http.HandleFunc("/api/data", func(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +29,7 @@ func main() {
 	})
 
 	log.Println("Server starting on port 8080")
-	err = http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
