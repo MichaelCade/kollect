e22f50006a05262dd1d2cc27d38f1d9c3f5a6efa
MichaelCade <michael.cade@outlook.com>
2024-08-17 12:39:58 +0100

    removed javascript and replaced with htmx

--- web/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    fetch('/api/data')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('data').textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => console.error('Error:', error));
-};
