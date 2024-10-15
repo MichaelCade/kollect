f3ae7cbd165f9bb5d1346bfe38d81903bd32e49b
MichaelCade <michael.cade@outlook.com>
2024-10-15 09:05:47 -0400

    changed to materialUI, added export button, added --browser function and --output

--- cmd/kollect/main.go
+++ cmd/kollect/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -15,29 +16,65 @@ import (
 func main() {
 	storageOnly := flag.Bool("storage", false, "Collect only storage-related objects")
 	kubeconfig := flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "Path to the kubeconfig file")
+	browser := flag.Bool("browser", false, "Open the web interface in a browser")
+	output := flag.String("output", "", "Output file to save the collected data")
 	flag.Parse()
 
+	data, err := collectData(*storageOnly, *kubeconfig)
+	if err != nil {
+		log.Fatalf("Error collecting data: %v", err)
+	}
+
+	if *output != "" {
+		err = saveToFile(data, *output)
+		if err != nil {
+			log.Fatalf("Error saving data to file: %v", err)
+		}
+		fmt.Printf("Data saved to %s\n", *output)
+		return
+	}
+
+	if *browser {
+		startWebServer(data)
+	} else {
+		printData(data)
+	}
+}
+
+func collectData(storageOnly bool, kubeconfig string) (interface{}, error) {
+	if storageOnly {
+		return kollect.CollectStorageData(kubeconfig)
+	}
+	return kollect.CollectData(kubeconfig)
+}
+
+func saveToFile(data interface{}, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(data)
+}
+
+func printData(data interface{}) {
+	// Use nushell or any other formatting tool to print data
+	prettyData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		log.Fatalf("Error formatting data: %v", err)
+	}
+	fmt.Println(string(prettyData))
+}
+
+func startWebServer(data interface{}) {
 	http.Handle("/", http.FileServer(http.Dir("web")))
 	http.HandleFunc("/api/data", func(w http.ResponseWriter, r *http.Request) {
-		var data interface{}
-		var err error
-
-		if *storageOnly {
-			data, err = kollect.CollectStorageData(*kubeconfig)
-		} else {
-			data, err = kollect.CollectData(*kubeconfig)
-		}
-
-		if err != nil {
-			log.Printf("Error collecting data: %v", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		err = json.NewEncoder(w).Encode(data)
+		err := json.NewEncoder(w).Encode(data)
 		if err != nil {
 			log.Printf("Error encoding data: %v", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
 		}
 	})
 
