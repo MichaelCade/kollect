2be16f70bfc0c68824f3693abb8f052ea714b5c3
MichaelCade <michael.cade@outlook.com>
2025-05-19 13:35:55 +0100

    untyped int constant, changed value to use float

--- pkg/cost/handler.go
+++ pkg/cost/handler.go
@@ -501,7 +501,7 @@ func GenerateMockResourceData(platform, resourceType string) map[string]interfac
 				"Location":      "eastus",
 				"Edition":       "Standard",
 				"DTU":           100,
-				"MaxSizeBytes":  float64(107374182400),
+				"MaxSizeBytes":  107.374,
 				"Status":        "Online",
 			},
 		}
